d153ff26e3207792c2130b3a301e075449dde65a
Chengyumeng <792400644@qq.com>
2018-04-21 22:29:43 +0800

    增加数据库的默认配置代码逻辑

--- config/config.go
+++ config/config.go
@@ -24,6 +24,14 @@ func NewDefaultConfiguration() *Configuration {
 		AppConfig: &AppConfig{
 			Address: "0.0.0.0:8080",
 			RunMode: "stage",
+			Database: &Database{
+				DriverName:"mysql",
+				User:"root",
+				Password:"0a1b2c3d4e",
+				Charset:"utf8",
+				Tns:"tcp(127.0.0.1:4540)/porkerface",
+				ShowSQL:false,
+			},
 		},
 	}
 }
--- initial/initial.go
+++ initial/initial.go
@@ -1,10 +1,14 @@
 package initial
 
-import "github.com/Chengyumeng/PokerFace/routes"
+import (
+	"github.com/Chengyumeng/PokerFace/routes"
+	"github.com/Chengyumeng/PokerFace/models"
+)
 
 func Run(config string) {
 	InitConfig(config)
 	InitDB()
+	models.InitModels()
 	InitEcho()
 	routes.InitRoutes()
 }
